6285c14f59d409db3886e456bb67132c3a595fc4
Jason Efstathiou <j@jason-e.dev>
2019-10-13 21:45:10 +0200

    add scaffold file

---
+++ src/components/.scaffold.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  div#scaffold
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  name: 'Scaffold',
+})
+export default class extends Vue {}
+</script>
+
+<style scoped src="./Sidebar.sass">
