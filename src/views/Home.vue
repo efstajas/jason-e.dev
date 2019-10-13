cb8dcd5325675a870262d1f8041ab4a776bb8f19
Jason Efstathiou <j@jason-e.dev>
2019-10-13 21:37:45 +0200

    remove default stuff

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,12 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+  div hello
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import { Component, Vue } from 'vue-property-decorator';
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld,
-  },
-};
+@Component({
+  name: 'Home',
+})
+export default class extends Vue {}
 </script>
