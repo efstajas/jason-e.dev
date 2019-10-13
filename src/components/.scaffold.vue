6efa08f1d5760547936e1617d60b829e0e98306e
Jason Efstathiou <j@jason-e.dev>
2019-10-13 21:48:05 +0200

    oops 2

--- src/components/.scaffold.vue
+++ src/components/.scaffold.vue
@@ -11,4 +11,4 @@ import { Component, Vue } from 'vue-property-decorator';
 export default class extends Vue {}
 </script>
 
-<style scoped src="./Scaffold.sass">
+<style scoped lang="sass" src="./Scaffold.sass">
