46b6a05633ec0e6db0198e25fa8c0308ee608e81
Jason Efstathiou <j@jason-e.dev>
2019-10-19 17:06:03 +0200

    add some projects to home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,36 @@
 <template lang="pug">
-  div#intro
-    p I'm a Product Owner, full-stack developer & UX designer.
-    p I'm currently working at Grover.
-    p Have a look at some selected projects:
+  div#Home
+    #intro
+      p I'm a Product Owner, full-stack developer & UX designer.
+      br
+      p I'm currently working at Grover.
+      p#haveALook Have a look at some selected projects:
+    #projects
+      ProjectListing(
+        title="Grover Loop"
+        date="March 2019"
+        role="System Architect & Product Manager"
+        subtitle="An endlessly flexible end-to-end subscription management platform."
+      )
 </template>
 
 <script>
 import { Component, Vue } from 'vue-property-decorator';
+import ProjectListing from '@/components/ProjectListing';
 
 @Component({
   name: 'Home',
+  components: {
+    ProjectListing,
+  },
 })
 export default class extends Vue {}
 </script>
+
+<style lang="sass" scoped>
+#intro
+  margin-bottom: 128px
+  #haveALook
+    margin-top: 64px
+#projects
+</style>
