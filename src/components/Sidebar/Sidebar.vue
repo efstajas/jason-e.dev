268cf12acb149b204c4ac8078ef57204dcc55826
Jason Efstathiou <j@jason-e.dev>
2019-10-19 17:05:47 +0200

    add Sidebar component

---
+++ src/components/Sidebar/Sidebar.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  div#Sidebar Jason Efstathiou
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  name: 'Sidebar',
+})
+export default class extends Vue {}
+</script>
+
+<style scoped lang="sass" src="./Sidebar.sass">
