5aa096a458f4a766cc320a46b4f7520ff2ee9baa
Jason Efstathiou <j@jason-e.dev>
2019-10-19 17:05:32 +0200

    add ProjectListing component

---
+++ src/components/ProjectListing/ProjectListing.vue
@@ -0,0 +1,40 @@
+<template lang="pug">
+  #ProjectListing
+    #main
+      p#title {{title}}
+      p#date {{date}}
+      p#role {{role}}
+      p#subtitle {{subtitle}}
+      #image
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component({
+  name: 'ProjectListing',
+})
+export default class extends Vue {
+  @Prop({
+    type: String,
+    required: true,
+  }) readonly title!: string;
+
+  @Prop({
+    type: String,
+    required: true,
+  }) readonly date!: string;
+
+  @Prop({
+    type: String,
+    required: true,
+  }) readonly role!: string;
+
+  @Prop({
+    type: String,
+    required: true,
+  }) readonly subtitle!: string;
+}
+</script>
+
+<style scoped lang="sass" src="./ProjectListing.sass">
