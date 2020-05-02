70436afdce65934625ef1471673eb5c02841e88d
Jason Efstathiou <j@jason-e.dev>
2020-05-02 15:18:47 +0200

    show loading indicator only if loading longer than 500ms

--- src/components/LoadingIndicator/LoadingIndicator.vue
+++ src/components/LoadingIndicator/LoadingIndicator.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-  div#LoadingIndicator(ref="container")
+  div#LoadingIndicator(ref="container" :class="{ appear }")
     #line(
       v-if="width"
       :style="{ '--parentWidth': `${width}px`, width: `${secondLineWidth}px` }"
@@ -17,8 +17,11 @@ export default class extends Vue {
 
   private secondLineWidth: number | null = null;
 
-  private async mounted() {
+  private appear: boolean = false;
+
+  private mounted() {
     this.calcWidths();
+    setTimeout(() => { this.appear = true; }, 500);
   }
 
   private calcWidths() {
