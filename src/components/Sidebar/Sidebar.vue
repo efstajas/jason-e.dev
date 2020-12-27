8d0ea8b3ac2c5ff01c304d3a08bfb618aa367905
Jason Efstathiou <j@jason-e.dev>
2020-12-27 11:38:38 +0100

    add sidebar progress ball

--- src/components/Sidebar/Sidebar.vue
+++ src/components/Sidebar/Sidebar.vue
@@ -14,19 +14,28 @@
           .title(:class="{ nudged: goHomeEnabled }") Jason Efstathiou
         .line
     scrollactive.chapters(
-      @itemchanged="activatedChapter = true"
+      @itemchanged="handleItemChanged"
     )
+      .progressBall(
+        :style="{ transform: `translateY(${ballYOffset}px) scale(${activeChapter ? 1 : 0})` }"
+      )
       .inner(
         name="fade"
         :style="{ width: width }"
       )
         a(
           class="scrollactive-item"
-          :class="{ 'is-active': !activatedChapter }"
+          :class="{ 'is-active': !activatedChapter, scrolledDown }"
           v-for="chapter, index in currentChapters"
           :key="chapter.hash"
           :href="`#${chapter.hash}`"
-        ) {{ chapter.text }}
+          :ref="chapter.hash"
+          :data-index="index"
+          :data-ref="chapter.hash"
+          @click="currentlyAutoscrolling = true"
+        )
+          .progressBar
+          span {{ chapter.text }}
 </template>
 
 <script lang="ts">
@@ -36,6 +45,26 @@ import { getModule } from 'vuex-module-decorators';
 import subChapters from '@/store/modules/subChapters/subChapters';
 
 import { Chapter } from '@/store/modules/subChapters/subChapters.types';
+
+function getChapterElements(el: Element, res: Element[] = []): Element[] {
+  const sibling = el.nextElementSibling;
+
+  if (!sibling) return res;
+  if (sibling.nodeName === 'H1') return res;
+
+  const newRes = [...res, sibling];
+
+  return getChapterElements(sibling, newRes);
+}
+
+function calculateChapterHeight(el: Element): number {
+  const chapterSiblings = getChapterElements(el);
+
+  const topPos = chapterSiblings[0].getBoundingClientRect().top;
+  const bottomPos = chapterSiblings.slice(-1)[0].getBoundingClientRect().bottom;
+
+  return bottomPos - topPos;
+}
 
 @Component({
   name: 'Sidebar',
@@ -47,6 +76,18 @@ export default class extends Vue {
 
   activatedChapter: boolean = false;
 
+  scrollY: number = 0;
+
+  activeChapter: HTMLAnchorElement | null = null;
+
+  activeChapterHeight: number | null = null;
+
+  activeChapterPos: number | null = null;
+
+  activeChapterScrollPercentage: number | null = null;
+
+  currentlyAutoscrolling: boolean = false;
+
   @Prop({
     required: true,
     type: Boolean,
@@ -57,8 +98,76 @@ export default class extends Vue {
     return this.chaptersModule.currentChapters;
   }
 
-  handleTitleClick():void {
+  handleTitleClick(): void {
     if (this.goHomeEnabled) this.$router.push({ name: 'projects' });
+  }
+
+  get scrolledDown(): boolean {
+    return this.scrollY > 0;
+  }
+
+  get ballYOffset(): number {
+    if (!this.activeChapter) return 0;
+
+    const isFirstChapter = parseInt(this.activeChapter.getAttribute('data-index')!, 10) === 0;
+    const isLastChapter = parseInt(this.activeChapter.getAttribute('data-index')!, 10) === this.currentChapters!.length - 1;
+
+    return this.activeChapter.offsetTop
+      + this.activeChapter.offsetHeight
+      * (isFirstChapter || isLastChapter
+        ? this.activeChapterScrollPercentage! / 2 + (isFirstChapter ? 0.5 : 0)
+        : this.activeChapterScrollPercentage!
+      ) - 5;
+  }
+
+  private updateScroll() {
+    if (!this.currentlyAutoscrolling) {
+      this.scrollY = window.scrollY;
+
+      if (this.activeChapter) {
+        this.activeChapterScrollPercentage = (
+          this.scrollY - this.activeChapterPos!
+        ) / this.activeChapterHeight!;
+      }
+    }
+  }
+
+  updateCurrentChapterHeightAndPos() {
+    if (!this.activeChapter) return;
+    const chapter = document.getElementById(this.activeChapter.getAttribute('data-ref')!)!;
+
+    this.activeChapterHeight = calculateChapterHeight(chapter);
+    this.activeChapterPos = chapter.offsetTop;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  registerNewChapter(el: HTMLElement) {
+    el.setAttribute('style', 'background-color: red;');
+    this.updateCurrentChapterHeightAndPos();
+  }
+
+  handleItemChanged(e: Event, n: HTMLAnchorElement | null, p: HTMLAnchorElement | null) {
+    this.activatedChapter = true;
+    this.activeChapter = n;
+    this.currentlyAutoscrolling = false;
+    this.updateCurrentChapterHeightAndPos();
+    this.updateScroll();
+  }
+
+  async mounted() {
+    this.updateScroll();
+
+    await this.$nextTick();
+
+    const observer = new MutationObserver(() => {
+      this.updateCurrentChapterHeightAndPos();
+    });
+
+    window.addEventListener('scroll', this.updateScroll);
+  }
+
+  destroyed() {
+    window.removeEventListener('scroll', this.updateScroll);
   }
 }
 </script>
